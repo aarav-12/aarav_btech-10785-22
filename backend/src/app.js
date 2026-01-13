bc8500e4d5000da6e857f314ee3ef41916400f2f
Aarav Mithrani <aarav.mi33@gmail.com>
2026-01-13 14:16:24 +0530

    task management done with git auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
 import express from "express";
 import session from "express-session";
+
 import passport from "./config/passport.js";
 import authRoutes from "./modules/auth/auth.routes.js";
 import taskRoutes from "./modules/tasks/task.routes.js";
+import { requireAuth } from "./middlewares/auth.middleware.js";
 
 const app = express();
 
@@ -20,6 +23,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/auth", authRoutes);
-app.use("/tasks", taskRoutes);
+
+app.use("/tasks", requireAuth, taskRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+app.get("/", (req, res) => {
+  res.send("Task Management API is running 🚀");
+});
+
 
 export default app;
